11ac44910a1ef9900627d73678cb6173d25b53b7
lis <foxigs@gmail.com>
2020-10-26 16:07:00 +0400

    create Card model

--- src/main/kotlin/ge/lis/sim/models/Card.kt
+++ src/main/kotlin/ge/lis/sim/models/Card.kt
@@ -1,9 +1,18 @@
 package ge.lis.sim.models
 
+import ge.lis.sim.timeNow
+
 
 /**
  * Created by
  * @Author: lis, Luganskiy Igor
  * Date: 23.10.2020  * Time: 23:04
  */
-data class Card(val name: String, val country: String, val operator: String, val expire: Long, val createdAt: Long)
+data class Card(val id: Long, val title: String, val country: String, val operator: String = "", var expire: Long = 0, var createdAt: Long = 0) {
+    constructor(id: Long, title: String, country: String, operator: String, expire: Long) : this(id, title, country) {
+        createdAt = timeNow()
+    }
+    constructor(id: Long, title: String, country: String, expire: Long) : this(id, title, country) {
+        createdAt = timeNow()
+    }
+}
