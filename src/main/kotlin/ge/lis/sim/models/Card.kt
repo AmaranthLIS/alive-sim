244f92aa3d6ae8063e2df1e178438eaad084a87f
lis <foxigs@gmail.com>
2020-10-26 16:30:46 +0400

    add serialize lib

--- src/main/kotlin/ge/lis/sim/models/Card.kt
+++ src/main/kotlin/ge/lis/sim/models/Card.kt
@@ -1,13 +1,15 @@
 package ge.lis.sim.models
 
 import ge.lis.sim.timeNow
-
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
 /**
  * Created by
  * @Author: lis, Luganskiy Igor
  * Date: 23.10.2020  * Time: 23:04
  */
+@Serializable
 data class Card(val id: Long, val title: String, val country: String, val operator: String = "", var expire: Long = 0, var createdAt: Long = 0) {
     constructor(id: Long, title: String, country: String, operator: String, expire: Long) : this(id, title, country) {
         createdAt = timeNow()
