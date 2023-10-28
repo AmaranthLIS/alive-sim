575d85c3dd5d41eb2c3d0b75359cf8e6cace944a
LIS <foxigs@gmail.com>
2023-10-29 02:03:54 +0400

    migrate gradle into 8.3 and use special config for kotlin

--- src/main/kotlin/ge/lis/sim/models/Card.kt
+++ src/main/kotlin/ge/lis/sim/models/Card.kt
@@ -1,14 +1,14 @@
 package ge.lis.sim.models
 
 import ge.lis.sim.today
-import kotlinx.serialization.*
+//import kotlinx.serialization.*
 
 /**
  * Created by
  * @Author: lis, Luganskiy Igor
  * Date: 23.10.2020  * Time: 23:04
  */
-@Serializable
+//@Serializable
 data class Card(val id: Long, val title: String, val country: String, val operator: String = "", var expire: Long = 0, var createdAt: Long = 0) {
     constructor(id: Long, title: String, country: String, operator: String, expire: Long) : this(id, title, country) {
         createdAt = today()
